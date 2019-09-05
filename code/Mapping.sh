cb18f3fe5c8585283feacf40a86df2a5601b010d
emilyli0325 <30696847+emilyli0325@users.noreply.github.com>
2019-09-05 15:39:47 -0500

    Update Mapping.sh

--- code/Mapping.sh
+++ code/Mapping.sh
@@ -9,13 +9,12 @@
 
 #files=(...)
 
-
 for file in ${files[*]}
 do
 
 echo "mapping $file..."
 
-bwa mem /master/xli/Index/Pfal32_bwa_index/Pfal32 fq/$file.R1.fastq.gz fq/$file.R2.fastq.gz -t 16 -M -R "@RG\tID:$file\tLB:$file\tPL:ILLUMINA\tPM:HISEQ\tSM:$file/" > SAM/$file.sam
+bwa mem ./Pfal32_bwa_index/Pfal32 fq/$file.R1.fastq.gz fq/$file.R2.fastq.gz -t 16 -M -R "@RG\tID:$file\tLB:$file\tPL:ILLUMINA\tPM:HISEQ\tSM:$file/" > SAM/$file.sam
 
 java -jar picard.jar SortSam \
      INPUT=SAM/$file.sam \
@@ -37,15 +36,15 @@ cd ..
 echo "BQSR $file..."	 
 	java -jar GenomeAnalysisTK.jar\
 		 -T BaseRecalibrator\
-		 -R ~/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
+		 -R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
 		 -I dedup.sorted.bam/$file.dedup.sorted.bam\
-		 -knownSites /master/xli/Index/Known_sites/3d7_hb3.combined.final.karo.sort.vcf\
-		 -knownSites /master/xli/Index/Known_sites/7g8_gb4.combined.final.karo.sort.vcf\
-		 -knownSites /master/xli/Index/Known_sites/hb3_dd2.combined.final.karo.sort.vcf\
+		 -knownSites ./Known_sites/3d7_hb3.combined.final.karo.sort.vcf\
+		 -knownSites ./Known_sites/7g8_gb4.combined.final.karo.sort.vcf\
+		 -knownSites ./Known_sites/hb3_dd2.combined.final.karo.sort.vcf\
 		 -o BQSR/$file.recal.table
-	java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar\
+	java -jar GenomeAnalysisTK.jar\
 		 -T PrintReads\
-		 -R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
+		 -R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
 		 -I dedup.sorted.bam/$file.dedup.sorted.bam\
 		 -BQSR BQSR/$file.recal.table\
 		 -o recal.bam/$file.recal.bam 
@@ -53,7 +52,7 @@ echo "BQSR $file..."
 echo "Variant calling $file..."
 	java -jar GenomeAnalysisTK.jar\
          -T HaplotypeCaller\
-         -R ~/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
+         -R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta\
          -I recal.bam/$file.recal.bam\
          --emitRefConfidence BP_RESOLUTION\
          -o g.vcf/$file.g.vcf\
