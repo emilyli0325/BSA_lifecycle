5365ef67af253368e7d7fec7dc464bfae091d895
emilyli0325 <30696847+emilyli0325@users.noreply.github.com>
2019-09-05 15:40:50 -0500

    Update genotype.and.filter.sh

--- code/genotype.and.filter.sh
+++ code/genotype.and.filter.sh
@@ -1,6 +1,6 @@
 ## GT and filter
 java -jar GenomeAnalysisTK.jar \
--T GenotypeGVCFs -R ~/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+-T GenotypeGVCFs -R ./PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
 -V BSA.g.vcf \
 --useNewAFCalculator \
 --sample_ploidy 1 \
@@ -8,29 +8,29 @@ java -jar GenomeAnalysisTK.jar \
 
 ## select good loci according to parent's genotype 
 # Seperate SNP and Indel
-java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
 -T SelectVariants \
--R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+-R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
 -V cross.052018.GATK.vcf \
 -selectType SNP -o cross.052018.SNP.vcf
 	
-java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
 -T SelectVariants \
--R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+-R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
 -V cross.052018.GATK.vcf  \
 -selectType INDEL -o cross.052018.INDEL.vcf
 
 # find SNPs called both parents and BSA-bulks    
-java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
    -T SelectVariants \
-   -R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+   -R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
    -V cross.052018.SNP.vcf \
    --concordance MKK2835.NHP1337.core.SNP.vcf \
    -o cross.052018.filter.vcf
 
-java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
    -T SelectVariants \
-   -R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+   -R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
    -V MKK2835.NHP1337.core.SNP.vcf \
    --concordance cross.052018.SNP.vcf \
    -o MKK2835.NHP1337.filter.vcf   
@@ -38,9 +38,9 @@ java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
    
 # vcf to table 
    
-java -jar /master/xli/software/GATK/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
 -T VariantsToTable \
--R /master/xli/Index/Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
+-R ./Pfal32_GATK_index/PlasmoDB-32_Pfalciparum3D7_Genome.fasta \
 -V cross.052018.filter.vcf \
 -F CHROM -F POS -F REF -F ALT \
 -GF AD -GF DP -GF GQ -GF PL \
